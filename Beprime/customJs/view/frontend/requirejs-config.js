9f2b37b29f59505f870b22c4847cde7753d56ebb
AndreaPizzi <45031370+AndreaPizzi@users.noreply.github.com>
2019-02-27 12:32:41 +0100

    First COmmit

---
+++ Beprime/customJs/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            berime_customjs:'Beprime/customJs/customjs',
+        }
+    }
+};
---
+++ Beprime/customJs/view/frontend/web/js/customjs.js
@@ -0,0 +1,8 @@
+
+require([
+"jquery"
+], function($){
+	$(document).ready(function() {
+        console.log('All right Sparky!!');
+	});
+});
